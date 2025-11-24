26b11fe9b636796d8b83996a17ed4a4b79465fe8
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-11-24 15:37:56 +0900

    test: モックのリクエストハンドラーを設定

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,16 @@
+import { rest } from "msw"
+
+export const handlers = [
+	rest.get(
+		"https://jsonplaceholder.typicode.com/users", // モックするAPIのURL。httpリクエストでhttps://jsonplaceholder.typicode.com/usersが呼ばれた場合に第二引数のresをreturnする
+		(req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				ctx.json([
+					{name: "John Doe"},
+					{name: "Test"}
+				])
+			)
+		}
+	)
+]
