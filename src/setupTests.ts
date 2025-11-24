04b21c13003ceec002cd4ead77d063559aee7167
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-11-24 15:37:33 +0900

    test: mswを使ったモックテストのセットアップ

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { server } from "@/mocks/server"
+
+
+// 全てのテスト前に必ず実行される（テストの初期設定）
+beforeAll(() => server.listen()) // mock serverを起動
+afterEach(() => server.resetHandlers()) // テスト終了後に毎回、mock serverをリセット
+afterAll(() => server.close()) // テスト終了後にmock serverを停止
